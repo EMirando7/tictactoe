84401136e597ed44aad2459eba316fb69f256bb9
Emiliano Miranda <emirando117@gmail.com>
2021-04-02 16:58:13 -0500

    Add squares and lines for game

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const gameBoard = {
-    let subGameboard =[];
+const tictactoe = {
+    initalize: (function () {
+        'use strict';
+        let gameBoard = [];
+
+    }),
+    //
+    players: function (player) {
+        //return what what is to be used in the newly created object
+        return {player}
+    }
 }
