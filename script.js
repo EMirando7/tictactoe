e36be07c19e53a84bcb8ee8882fc639f61e27889
Emiliano Miranda <emirando117@gmail.com>
2021-04-04 00:31:16 -0500

    Correct the passing of variables amonst functions; complete first two funcs

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const tictactoe = {
-    initalize: (function () {
-        'use strict';
-        let gameBoard = [];
-
+const game = {
+    //invoke initalize
+    initialize: (function () {
+        //hides the X from board
+        const hideX = (function () {
+            let boardXs = document.querySelectorAll('#ghost > div');
+            for (let i = 0; i < boardXs.length; i++) {
+                boardXs[i].innerHTML = " ";
+            }
+            //object shorthand
+            return { boardXs };
+        })();
+        //hears for clicks, changes to depending on which player clicks
+        (function toggleSymbol() {
+            console.log(hideX)
+        })()
     }),
-    //
     players: function (player) {
         //return what what is to be used in the newly created object
-        return {player}
+        return { player }
     }
 }
