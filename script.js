7ac746e1d369b6a449e75bb1d1679f288e748e37
Emiliano Miranda <emirando117@gmail.com>
2021-04-04 01:19:16 -0500

    Move boardXs to outerscope instead of returning object shorthand

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const game = {
     //invoke initalize
     initialize: (function () {
+        let boardXs = document.querySelectorAll('#ghost > div');
         //hides the X from board
-        const hideX = (function () {
-            let boardXs = document.querySelectorAll('#ghost > div');
+        const resultX = (function hideX() {
             for (let i = 0; i < boardXs.length; i++) {
                 boardXs[i].innerHTML = " ";
             }
-            //object shorthand
-            return { boardXs };
         })();
         //hears for clicks, changes to depending on which player clicks
-        (function toggleSymbol() {
-            console.log(hideX)
+        const toggleSymbol = (function () {
+            boardXs.forEach(element => {
+                element.addEventListener('click', () => {
+                    element.innerHTML = "X"
+                })
+            });
         })()
     }),
     players: function (player) {
